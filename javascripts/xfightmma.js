6cb4964bb0c2286530f0213549ed3ac0ff427afa
Leonardo Comelli <leonardo.comelli@gmail.com>
2014-08-06 00:03:51 -0300

    fix facebook plugin

--- javascripts/xfightmma.js
+++ javascripts/xfightmma.js
@@ -11,3 +11,24 @@ $( document ).ready(function() {
     window.open('http://goo.gl/cwYKNW')
   });
 });
+
+/* Facebook plugin */
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=158315454324166";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/* Google Analytics */
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39084153-1']);
+  _gaq.push(['_setDomainName', 'xfightmma.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
